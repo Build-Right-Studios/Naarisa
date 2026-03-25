f754f0e4ab63dbec8e701866f0b183cde8b8660b
Aryesh02 <aryeshsrivastava@gmail.com>
2026-03-25 12:56:55 +0530

    Added Product Module

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import authRoutes from "./Modules/Auth/auth.routes.js";
+import userRoutes from "./Modules/User/user.routes.js";
+import productRoutes from "./Modules/Product/product.routes.js";
 
 const router = express.Router();
 
 router.use("/api/auth", authRoutes);
 router.use("/api/user", userRoutes);
+router.use("/api/product", productRoutes);
+
 export default router;
