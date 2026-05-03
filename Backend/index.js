27a8b2e72263a127786bc4a0da30e11ea229a3f7
Aryesh02 <aryeshsrivastava@gmail.com>
2026-05-03 19:33:17 +0530

    Changes to Backend

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    'http://localhost:5173',
+    'https://naarisa-admin-git-internal-aryeshs-projects.vercel.app',
+    'https://naarisa-admin.vercel.app'
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
