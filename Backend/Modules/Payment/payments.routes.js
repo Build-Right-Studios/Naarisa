82957b4087e5566c8fe0b288fc98331479d746c0
Aryesh02 <aryeshsrivastava@gmail.com>
2026-07-05 01:25:49 +0530

    Changes to Webhook

--- Backend/Modules/Payment/payments.routes.js
+++ Backend/Modules/Payment/payments.routes.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { verifyPayment } from "./Controller/verifyPayment.js";
-import { razorpayWebhook } from "./Controller/razorpayWebhookController.js";
 
 const router = express.Router();
 
 router.post("/verify-payment", verifyPayment);
-router.post("/razorpay", express.raw({ type: "application/json" }), razorpayWebhook);
 export default router;
